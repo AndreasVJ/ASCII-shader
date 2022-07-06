40dfbc846e33b753a6e6aef03701f85524dde51e
Andreas Jonsterhaug <a.jonsterhaug@gmail.com>
2022-07-06 22:53:52 +0200

    ui improvements

--- script.js
+++ script.js
@@ -1,70 +1,84 @@
 const imageUploader = document.getElementById("imageUploader")
 const inputImage = document.getElementById("inputImage")
 const video = document.getElementById("video")
-const startCameraButton = document.getElementById("startCameraButton")
-const stopCameraButton = document.getElementById("stopCameraButton")
+const cameraButton = document.getElementById("cameraButton")
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 const ascii = document.getElementById("ascii")
+const resolutionSlider = document.getElementById("resolutionSlider")
+const resolutionSliderText = document.getElementById("resolutionSliderText")
+
+
+resolutionSliderText.innerText = "Pixelation: " + resolutionSlider.value
+resolutionSlider.oninput = () => {
+    resolutionSliderText.innerText = "Pixelation: " + resolutionSlider.value
+}
 
 let streaming = false
 
-ctx.scale(-1, 1);
-ctx.font = '48px serif';
-ctx.fillText('Hello world!', -280, 90);
-
 imageUploader.onchange = event => {
+
+    stopCamera()
+    cameraButton.onclick = startCamera
+
+    ascii.classList.remove("flipHorizontally")
+
     inputImage.src = URL.createObjectURL(event.target.files[0])
     inputImage.onload = () => {
         URL.revokeObjectURL(inputImage.src) // free memory
 
-        const size = 2
+        const imageData = getImageDataFromImage(inputImage, inputImage.width, inputImage.height)
 
-        let imageData = getImageDataFromImage(inputImage, inputImage.width, inputImage.height)
-
-        ctx.scale(-1, 1)
-        ctx.putImageData(imageData, 0, 0)
-
-        const text = convertImageDataToASCII(imageData, size)
+        const text = convertImageDataToASCII(imageData, parseInt(resolutionSlider.value))
+        ascii.style.fontSize = window.innerHeight*0.7 / (inputImage.height / resolutionSlider.value) + "px"
         ascii.innerHTML = text
     }
 }
 
 function startCamera() {
+
+    cameraButton.onclick = stopCamera
+
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then( stream => {
         video.srcObject = stream
+        video.msHorizontalMirror = true;
         video.play()
         streaming = true
-        setTimeout(timerCallback, 2000)
     })
     .catch( error => {
-        alert("Det har oppstått en feil: " + error)
+        alert("An error has occurred: " + error)
     })
 }
 
 function stopCamera() {
+
+    cameraButton.onclick = startCamera
+
     video.srcObject.getTracks().forEach(function(track) {
         track.stop()
     })
     streaming = false
 }
 
-startCameraButton.onclick = startCamera
-stopCameraButton.onclick = stopCamera
+cameraButton.onclick = startCamera
 
 function timerCallback() {
     if (!streaming) {
         return;
     }
-    const imageData = getImageDataFromImage(video, video.videoWidth, video.videoHeight)
-    const text = convertImageDataToASCII(imageData, 10)
-    ascii.innerHTML = text
+    if (video.videoWidth != 0) {
+        const imageData = getImageDataFromImage(video, video.videoWidth, video.videoHeight)
+        const text = convertImageDataToASCII(imageData, parseInt(resolutionSlider.value))
+        ascii.style.fontSize = window.innerHeight*0.7 / (video.videoHeight / resolutionSlider.value) + "px"
+        ascii.innerHTML = text
+    }
     setTimeout(() => {
         timerCallback()
     }, 0)
 }
 
 video.addEventListener("play", () => {
-    console.log("test")
+    timerCallback()
+    ascii.classList.add("flipHorizontally")
 })
